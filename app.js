061325fda26288a83ac365d817df008190af844d
nolangh <nolanghiggins@gmail.com>
2022-05-21 18:52:44 -0600

    finished

--- app.js
+++ app.js
@@ -68,3 +68,57 @@ app.set("view engine", "hbs");
 //-----------------------------------------------------
 
 // ANCHOR partials for HandleBars
+hbs.registerPartials(path.join(__dirname, "views/partials"));
+// --------------------------------------------------
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// ANCHOR Configure session, passport, flash
+// --------------------------------------------------
+app.use(
+	session({
+		secret: "session secret",
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use((req, res, next) => {
+	res.locals.loggedIn = req.isAuthenticated();
+	next();
+});
+// --------------------------------------------------
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// ANCHOR Add new routes
+// --------------------------------------------------
+app.use("/auth", authRouter);
+// --------------------------------------------------
+
+// ANCHOR catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// ANCHOR error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	// ANCHOR render the error page
+	res.status(err.status || 500);
+	res.render("error");
+});
+
+module.exports = app;
